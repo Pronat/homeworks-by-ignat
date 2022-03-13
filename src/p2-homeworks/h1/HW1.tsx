31f3803a6fd7f12347a095e2aea10561973b1f21
AlexPron <Fotolinza@gmail.com>
2022-03-13 22:52:58 +0300

    task-1.2

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Message from "./Message";
+
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+    name: 'Artem',
+    message: 'npm start нажимал?',
+    time: '20:00',
 }
 
 function HW1() {
@@ -15,12 +17,12 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+ export type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+    }
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
+        <div className={s.container}>
+            <img src={props.avatar} alt={'avatarImage'} className={s.avatar} />
+
+            <div className={s.angle} />
+
+            <div className={s.content}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.text}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
 
         </div>
     )
