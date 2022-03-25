91a1c9d7c26aa7cd77b589143b57d3805b4a2a43
AlexPron <Fotolinza@gmail.com>
2022-03-25 21:30:52 +0300

    task-2.5

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -33,15 +33,15 @@ export const filterAffairs = (affairs: AffairType[], filter: FilterType): Affair
     // need to fix
 }
 export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => { // need to fix any
-    return  affairs.filter(a => _id !== _id)// need to fix
+    return  affairs.filter(a => a._id !== _id)// need to fix
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => setAffairs(deleteAffair(affairs, _id)) // need to fix any
+    const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id)) // need to fix any
 
     return (
         <div>
