2f0ab93d7cb6bc853a3e5321c482629fbaaf19c6
AlexPron <Fotolinza@gmail.com>
2022-04-15 23:29:50 +0300

    task-3.4

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
+    name: string // need to fix any
     setNameCallback: any // need to fix any
-    addUser: any // need to fix any
+    addUser: () => void // need to fix any
     error: any // need to fix any
     totalUsers: any // need to fix any
 }
