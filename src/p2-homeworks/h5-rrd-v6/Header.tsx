c9d04b4a5d9deae3a0db2d0de69482427c4ff35b
Pronat <Fotolinza@gmail.com>
2022-04-29 23:04:06 +0300

    task-5.3

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
         <div>
+            <NavLink to = {'/pre-junior'} />pre-junior</NavLink>
             // add NavLinks
 
         </div>
