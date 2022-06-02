31267db80f8485d1cea9bd4150f5a73fdecbf3d3
Pronat <Fotolinza@gmail.com>
2022-06-02 20:00:15 +0300

    task6-1

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -10,6 +10,7 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
+        restoreState<string>('editable-span-value', value)
         // setValue()
     }
 
