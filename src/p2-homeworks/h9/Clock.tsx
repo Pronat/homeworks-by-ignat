5f0fb0335a64bfc23d7f5f5fee5a3cfed120fe5c
AlexPron <Fotolinza@gmail.com>
2022-07-23 16:47:07 +0300

    task9-3

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {clearInterval} from "timers";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -7,27 +8,31 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        clearInterval(timerId)
-        // stop
+        clearInterval()
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
         // show
+        setShow(true)
 
     }
     const onMouseLeave = () => {
         // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    // const stringTime = 'Time' // fix with date
+    const stringTime = date?.toLocaleTimeString() || <br/>
+    // const stringDate = 'Date' // fix with date
+    const stringDate = date?.toLocaleDateString() || <br/>
 
     return (
         <div>
