a9aa8e284d614c96ee124064b674fdf50110b24b
AlexPron <Fotolinza@gmail.com>
2022-09-11 20:01:08 +0300

    task11-1

--- src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import HW10 from "../../h10/HW10";
+import HW11 from "../../h11/HW11";
 
 function JuniorPlus() {
     return (
         <div>
             JuniorPlus
             <HW10/>
-            {/*<HW11/>*/}
+            <HW11/>
             {/*<HW12/>*/}
         </div>
     )
