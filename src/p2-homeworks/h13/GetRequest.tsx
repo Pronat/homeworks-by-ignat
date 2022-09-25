860b583f1018c61cb7274d49fad600c56a830cbb
AlexPron <Fotolinza@gmail.com>
2022-09-25 20:22:04 +0300

    task13-6

--- src/p2-homeworks/h13/GetRequest.tsx
+++ src/p2-homeworks/h13/GetRequest.tsx
@@ -7,8 +7,10 @@ const instance = axios.create({
 
 export const GetRequest = () => {
     const [state, setState] = useState<any>(null)
+    const [value, setValue] = useState<boolean>(false)
+
     const getFromButton = () => {
-        let promise = axios.get("https://neko-cafe-back.herokuapp.com/auth/test")
+        let promise = axios.post("https://neko-cafe-back.herokuapp.com/auth/test", {success: value})
         promise.then((res) => {
             setState(res.data)
         })
@@ -17,7 +19,7 @@ export const GetRequest = () => {
     return <div>
         {JSON.stringify(state)}
         <div>
-            <input type={"checkbox"}/>
+            <input type={"checkbox"} checked={value} onChange={(e) => setValue(e.currentTarget.checked)}/>
             <button onClick={getFromButton}>button</button>
         </div>
 
